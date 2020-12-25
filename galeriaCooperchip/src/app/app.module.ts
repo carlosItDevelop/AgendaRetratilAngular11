19f522a59bae2ad364c0809b0b1d9d4b87f59830
Carlos A Santos <carlos.itdeveloper@gmail.com>
2020-12-25 20:41:54 -0300

    add json-server and move .git to root folder

---
+++ galeriaCooperchip/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
+import { PacienteListaComponent } from './components/paciente/paciente-lista/paciente-lista.component';
+import { PacienteReadComponent } from './components/paciente/paciente-read/paciente-read.component';
+import { MedicoListaComponent } from './components/medico/medico-lista/medico-lista.component';
+import { MedicoReadComponent } from './components/medico/medico-read/medico-read.component';
+import { PacienteCreateComponent } from './components/paciente/paciente-create/paciente-create.component';
+import { PacienteDeleteComponent } from './components/paciente/paciente-delete/paciente-delete.component';
+import { NavComponent } from './template/nav/nav.component';
+import { HeaderComponent } from './template/header/header.component';
+import { ContentComponent } from './template/content/content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PacienteListaComponent,
+    PacienteReadComponent,
+    MedicoListaComponent,
+    MedicoReadComponent,
+    PacienteCreateComponent,
+    PacienteDeleteComponent,
+    NavComponent,
+    HeaderComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
